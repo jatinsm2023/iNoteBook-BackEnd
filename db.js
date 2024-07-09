f364f92953c9c2afda7014639680c9632baa7b58
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-07-09 15:28:17 +0530

    mongodb connected

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/inotebook';
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
